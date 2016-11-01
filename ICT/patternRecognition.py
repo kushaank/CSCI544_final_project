0adbf0992b59742c8999f0ad0618cda38f22d3f5
Kushaan Kumar <kushaank@usc.edu>
2016-10-31 18:30:18 -0700

    basic extraction for given verb

--- ICT/patternRecognition.py
+++ ICT/patternRecognition.py
@@ -2,30 +2,59 @@ import csv
 import fnmatch
 import os
 from collections import defaultdict
-import nltk.data
+from nltk.stem.wordnet import WordNetLemmatizer
 
-filePathDictionary = defaultdict(int)
+def getAbsolutePath():
+    '''
+    Create map between file name and the absolute file path of every news file
+    :return: absolute path
+    '''
+    filePathDictionary = defaultdict(int)
+    for root, dirnames, filenames in os.walk("NewsTextFiles"):
+        for filename in fnmatch.filter(filenames, '*.txt'):
+            relativePath = os.path.join(root, filename)
+            fullFilePath = os.path.abspath(relativePath)
+            fileNameWithoutExtension = os.path.splitext(filename)[0].strip()
+            filePathDictionary[fileNameWithoutExtension] = fullFilePath
+    return filePathDictionary
 
-for root, dirnames, filenames in os.walk("NewsTextFiles"):
-    for filename in fnmatch.filter(filenames, '*.txt'):
-        # get the full file path
-        relativePath = os.path.join(root, filename)
-        fullFilePath = os.path.abspath(relativePath)
-        fileNameWithoutExtension = os.path.splitext(filename)[0].strip()
-        filePathDictionary[fileNameWithoutExtension] = fullFilePath
+def extractSentencesForVerb(target_verb):
+    '''
+    Get all files associated with the given verb
+    :param verb: the verb for which files need to be extracted
+    :return: all the files associated with the verb
+    '''
+    filePathDictionary = getAbsolutePath()
+    mycsv = csv.reader(open("finalPredicates3cols.csv"))
+    dict = {}
+    count = 0
+    for row in mycsv:
+        if count == 0:
+            #splits the csv file by the period delimeter to get the verb associated
+            verb = row[1].split(".")[0].strip()
+            fileName = row[len(row) - 1].strip()
+            if fileName in filePathDictionary:
+                if verb == target_verb:
+                    # get all the words in the file
+                    file = open(filePathDictionary[fileName], "r")
+                    sentences = file.read().split('\n')
+                    print sentences
+                    for sentenceNum in range(len(sentences)):
+                        dictKey = fileName + "_Sent:" + str(sentenceNum)
 
-mycsv = csv.reader(open("finalPredicates3cols.csv"))
-for row in mycsv:
-    verb = row[1].split(".")[0].strip()
-    fileName = row[len(row) - 1].strip()
-    if fileName in filePathDictionary:
-        if verb == "claim":
-            i=1
+                        sentence = sentences[sentenceNum]
+                        rootVerb = WordNetLemmatizer().lemmatize(target_verb,'v')
+                        print rootVerb
+                        if rootVerb in sentence.split() and dictKey not in dict:
+                            dict[dictKey] = sentences[sentenceNum]
+        count+=1
 
-text = "my name is eamon. hello how are you"
-sent_detector = nltk.data.load('tokenizers/punkt/english.pickle')
-delimeter = '\n-----\n'
-sentenceWithDelimeters = delimeter.join(sent_detector.tokenize(text.strip()))
-sentences = sentenceWithDelimeters.split(delimeter)
-for sentence in sentences:
-    print(sentence)
+    return dict
+
+
+def main():
+    print extractSentencesForVerb("accuse")
+
+main()
+
+
