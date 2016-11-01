7a42f256fca4af4c6d63d262c2337982236637c3
Eamon Barkhordarian <ebarkhor@usc.edu>
2016-10-31 17:11:02 -0700

    can split english sentences

--- ICT/patternRecognition.py
+++ ICT/patternRecognition.py
@@ -1,15 +1,14 @@
 import csv
-import sys
-import os
 import fnmatch
+import os
 from collections import defaultdict
-
+import nltk.data
 
 filePathDictionary = defaultdict(int)
 
 for root, dirnames, filenames in os.walk("NewsTextFiles"):
     for filename in fnmatch.filter(filenames, '*.txt'):
-        #get the full file path
+        # get the full file path
         relativePath = os.path.join(root, filename)
         fullFilePath = os.path.abspath(relativePath)
         fileNameWithoutExtension = os.path.splitext(filename)[0].strip()
@@ -18,10 +17,15 @@ for root, dirnames, filenames in os.walk("NewsTextFiles"):
 mycsv = csv.reader(open("finalPredicates3cols.csv"))
 for row in mycsv:
     verb = row[1].split(".")[0].strip()
-    fileName = row[len(row)-1].strip()
+    fileName = row[len(row) - 1].strip()
     if fileName in filePathDictionary:
         if verb == "claim":
-            #print("YES")
-        
+            i=1
 
-# print(allFiles)
+text = "my name is eamon. hello how are you"
+sent_detector = nltk.data.load('tokenizers/punkt/english.pickle')
+delimeter = '\n-----\n'
+sentenceWithDelimeters = delimeter.join(sent_detector.tokenize(text.strip()))
+sentences = sentenceWithDelimeters.split(delimeter)
+for sentence in sentences:
+    print(sentence)
