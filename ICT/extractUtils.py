b2541c305656fea7cf71339dabf40799db8178f7
Kushaan Kumar <kushaank@usc.edu>
2016-11-01 21:09:53 -0700

    got SRL parsing to work through pandas dataframe to get sentences

--- ICT/extractUtils.py
+++ ICT/extractUtils.py
@@ -2,62 +2,81 @@ import csv
 import fnmatch
 import os
 from collections import defaultdict
-import en
-import nltk
 import io
 from getBaseWord import getBase
+import numpy as np
+import pandas as pd
+from io import StringIO
 
-def getAbsolutePath():
+def getAbsolutePath(fileType, directoryName):
     '''
     Create map between file name and the absolute file path of every news file
     :return: absolute path
     '''
     filePathDictionary = defaultdict(int)
-    for root, dirnames, filenames in os.walk("NewsTextFiles"):
-        for filename in fnmatch.filter(filenames, '*.txt'):
+    for root, dirnames, filenames in os.walk(directoryName):
+        for filename in fnmatch.filter(filenames, '*' + fileType):
             relativePath = os.path.join(root, filename)
             fullFilePath = os.path.abspath(relativePath)
             fileNameWithoutExtension = os.path.splitext(filename)[0].strip()
             filePathDictionary[fileNameWithoutExtension] = fullFilePath
     return filePathDictionary
 
-def extractSentencesForVerb(target_verb):
+def getAbsolutePathForTargetVerb(files, fileType, directoryName):
+    '''
+    Create map between file name and the absolute file path of every news file
+    :return: absolute path
+    '''
+    filePathDictionary = defaultdict(int)
+    for root, dirnames, filenames in os.walk(directoryName):
+        for filename in fnmatch.filter(filenames, '*' + fileType):
+            fileNameWithoutExtension = os.path.splitext(filename)[0].strip()
+            if fileNameWithoutExtension in files:
+                relativePath = os.path.join(root, filename)
+                fullFilePath = os.path.abspath(relativePath)
+                filePathDictionary[fileNameWithoutExtension] = fullFilePath
+    return filePathDictionary
+
+def getFileNamesForVerb(target_verb):
     '''
     Get all files associated with the given verb
     :param verb: the verb for which files need to be extracted
     :return: all the files associated with the verb
     '''
-    filePathDictionary = getAbsolutePath()
+    filePathDictionary = getAbsolutePath(".txt", "NewsTextFiles")
     mycsv = csv.reader(open("finalPredicates3cols.csv"))
-    dict = {}
-    count = 0
+
+    files = []
+
     for row in mycsv:
         #splits the csv file by the period delimeter to get the verb associated
         verb = row[1].split(".")[0].strip()
         fileName = row[len(row) - 1].strip()
         if fileName in filePathDictionary:
             if verb == target_verb:
-                # get all the words in the file
-                file = io.open(filePathDictionary[fileName], "r",encoding='utf-8')
-
-                sentences = file.read().split("\n\n")
-                sentences[:] = (value for value in sentences if value != '\t')
-
-                for sentenceNum in range(len(sentences)):
-                    dictKey = fileName
-                    sentence = sentences[sentenceNum]
-                    if getBase(sentence,target_verb) is True:
-                        if dictKey not in dict:
-                            dict[dictKey] = []
-                            dict[dictKey].append(sentenceNum+1)
-                        else:
-                            dict[dictKey].append(sentenceNum+1)
-
-    return dict
-
+                files.append(fileName+".txt")
+    return files
 
 def main():
-    print extractSentencesForVerb("accuse")
+    files = getFileNamesForVerb("accuse")
+    dict = getAbsolutePathForTargetVerb(files, ".srl", "ClearnlpOutput")
+
+    sentences = []
+    for key in dict.keys():
+        file = io.open(dict[key], "r", encoding='utf-8')
+        srlSentenceChunks = file.read().split("\n\n")
+
+        for chunk in srlSentenceChunks[:-1]:
+            TESTDATA = StringIO(chunk)
+            df = pd.read_table(TESTDATA, names=["ID","Word","Lemma","POS","Features","Parent","Dependency Labels","SRL"])
+            if "accuse" in df["Lemma"].tolist():
+                sentences.append(df)
+
+        break
+
+
+
+
 
 main()
 
