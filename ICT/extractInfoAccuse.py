392ef41a19f7b76109a1f61673c4de1ca91ec9e9
Kushaan Kumar <kushaank@usc.edu>
2016-11-07 16:43:22 -0800

    accuse almost works

---
+++ ICT/extractInfoAccuse.py
@@ -0,0 +1,120 @@
+import extractUtils
+import pandas as pd
+from io import StringIO
+import io
+import calendar
+
+def main():
+    targetVerb = "accuse"
+    files = extractUtils.getFileNamesForVerb(targetVerb)
+    dict = extractUtils.getAbsolutePathForSrlFiles(files, "ClearnlpOutput")
+
+    ID = "ID"
+    WORD = "Word"
+    LEMMA = "Lemma"
+    POS = "POS"
+    FEATURES = "Features"
+    PARENT = "Parent"
+    DEPENDENCY_LABELS = "Dependency Labels"
+    SRL = "SRL"
+
+    A0 = "A0"
+    A1 = "A1"
+
+    sentences = []
+    for key in dict.keys():
+
+        path = "/Users/kushaankumar/Desktop/CSCI544_final_project/ICT/ClearnlpOutput/ClearnlpOutput/Part8/newsText4448.txt.srl"
+        file = io.open(path,"r", encoding='utf-8')
+        srlSentenceChunks = file.read().split("\n\n")
+
+        for chunk in srlSentenceChunks[:-1]:
+            TESTDATA = StringIO(chunk)
+            df = pd.read_table(TESTDATA, names=[ID, WORD, LEMMA, POS, FEATURES, PARENT, DEPENDENCY_LABELS, SRL])
+            if targetVerb in df[LEMMA].tolist():
+                sentences.append(df)
+            break
+        break
+
+    resultsDictionary = {}
+    resultsDictionary["action"] = targetVerb
+    # search for row which has the target verb
+    targetVerbRow = df.loc[df[LEMMA] == targetVerb]
+
+    # grab the ID of that verb at that row
+    targetVerbID = str(targetVerbRow[ID].iloc[0])
+
+    # tracks the ID for the word "of" to extract the possible action from the subtree of that node
+    IDForOf = -1
+
+    # look at the SRL column in this chunk to see which row has the target verb identification as an argument
+    for index, row in df.iterrows():
+        srl = df.get_value(index, SRL)
+
+        twoOrMoreSRLArguments = str(srl).split(";")  # 3:A0=PAG;11:A0=PAG two or more arguments are separated by semicolon
+
+        oneSRLArgument = str(srl).split(":")  # 28:A0=PAG one argument separates relatedID and argument with colon
+
+        validSRLs = []
+        if len(twoOrMoreSRLArguments) > 1:  # there are two or more arguments
+            validSRLs = twoOrMoreSRLArguments
+        elif len(oneSRLArgument) > 1:  # one argument
+            validSRLs.append(srl)
+
+        for srlSection in validSRLs:  # ["3:A0=PAG", "11:A0=PAG"]
+            argumentSplit = srlSection.split(":")
+            relatedID = str(argumentSplit[0])  # 3
+            argumentNumberFull = argumentSplit[1]  # 'A0=PAG'
+
+            if relatedID == targetVerbID:  # our current row has an agent that corresponds to our target action
+                argumentNumber = argumentNumberFull.split("=")[0]  # just want to extract the 'A0' from 'A0=PAG'
+                agentID = str(df.get_value(index, ID))  # the ID of the row of the SRL with the agent
+
+                # need more children examples
+                # childrenDictionary = {}
+
+                # for index1, row1 in df.iterrows():
+                #     parent = df.get_value(index1, PARENT)
+                #     if str(parent) == agentID: #a row's parent value is the ID of the agent we found
+                #         print df.get_value(index1, ID)
+                #         childrenDictionary[df.get_value(index1, ID)] = str(df.get_value(index1, WORD))
+
+                word = df.get_value(index, WORD)
+                resultsDictionary[argumentNumber] = word
+                resultsDictionary[argumentNumber] = extractUtils.getFullAgent(df, agentID)
+
+                # extracting the ID for of to find possible action
+                if argumentNumber == "A2":
+                    IDForOf = row[ID]
+
+                ##get the child of the node 'accused', usually called "of", then get child of that to obtain the possible action/ action
+
+
+        #if the correspoinding row has the preposition "of" get the possible action which is A2 for the target action
+        if row[PARENT] == int(IDForOf):
+            resultsDictionary["possible action"] = resultsDictionary["A2"]
+
+        #check if this is a passive action
+        if row[WORD] == "are":
+            if int(df.get_value(index, PARENT)) == int(targetVerbID):
+                resultsDictionary["passive"] = "true"
+
+        # see if the word corresponds to any day/month and if yes add it to the date/location param
+        if row[WORD] in calendar.day_name:
+            resultsDictionary["Date/Time"] = row[WORD]
+
+        if row[WORD] in calendar.month_name:
+            resultsDictionary["Date/Time"] = row[WORD]
+
+        if row[WORD] in calendar.day_abbr:
+            resultsDictionary["Date/Time"] = row[WORD]
+
+        if row[WORD] in calendar.month_abbr:
+            resultsDictionary["Date/Time"] = row[WORD]
+
+    print resultsDictionary
+    # print childrenDictionary
+
+
+main()
+
