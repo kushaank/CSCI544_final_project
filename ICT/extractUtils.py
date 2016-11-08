8adcc3957ce7913619e3e5239f8ccab3ad03edd9
Eamon Barkhordarian <ebarkhor@usc.edu>
2016-11-07 16:35:38 -0800

    removed some comments from extract utils methods

--- ICT/extractUtils.py
+++ ICT/extractUtils.py
@@ -86,12 +86,12 @@ def getFullAgent(df, targetParentID):
     given a certain node, returns a string of the agent and all its child nodes in order
     :return: String
     '''
+    targetParentId = str(targetParentID)
     childNodeDictionary = {}
     dictionary = {}
     for index, row in df.iterrows():
         currentID = str(df.get_value(index, ID))
         if currentID == targetParentID: #a row's parent value is the ID of the agent we found
-            print currentID
             targetWord = str(df.get_value(index, WORD))
             dictionary[targetParentID] = targetWord
             childNodeDictionary = getChildNodes(df, targetParentID, dictionary)
@@ -111,7 +111,6 @@ def getChildNodes(df, targetParentID, dictionary):
                 dictionary.update(getChildNodes(df, currentID, dictionary))
         return dictionary
     else:
-        print dictionary
         return dictionary
 
 
@@ -127,60 +126,56 @@ def getFullAgentFromChildNodes(dictionary):
     return " ".join(sortedValues)
 
 def main():
-    targetVerb = "evacuate"
-    files = getFileNamesForVerb(targetVerb)
-    dict = getAbsolutePathForSrlFiles(files, "ClearnlpOutput")
+    # targetVerb = "evacuate"
+    # files = getFileNamesForVerb(targetVerb)
+    # dict = getAbsolutePathForSrlFiles(files, "ClearnlpOutput")
 
-    sentences = []
-    for key in dict.keys():
-        file = io.open("/Users/eamonb/Documents/CS544/Projects/group project/CSCI544_final_project/ICT/ClearnlpOutput/ClearnlpOutput/Part30/newsText17789.txt.srl", "r", encoding='utf-8')
-        srlSentenceChunks = file.read().split("\n\n")
+    # sentences = []
+    # for key in dict.keys():
+    #     file = io.open("/Users/eamonb/Documents/CS544/Projects/group project/CSCI544_final_project/ICT/ClearnlpOutput/ClearnlpOutput/Part30/newsText17789.txt.srl", "r", encoding='utf-8')
+    #     srlSentenceChunks = file.read().split("\n\n")
 
-        for chunk in srlSentenceChunks[:-1]:
-            TESTDATA = StringIO(chunk)
-            df = pd.read_table(TESTDATA, names=[ID,WORD,LEMMA,POS,FEATURES,PARENT,DEPENDENCY_LABELS,SRL])
-            if targetVerb in df[LEMMA].tolist():
-                sentences.append(df)
-            break
-        break
+    #     for chunk in srlSentenceChunks[:-1]:
+    #         TESTDATA = StringIO(chunk)
+    #         df = pd.read_table(TESTDATA, names=[ID,WORD,LEMMA,POS,FEATURES,PARENT,DEPENDENCY_LABELS,SRL])
+    #         if targetVerb in df[LEMMA].tolist():
+    #             sentences.append(df)
+    #         break
+    #     break
     
-    resultsDictionary = {}
-    resultsDictionary["action"] = targetVerb
-    #search for row which has the target verb
-    targetVerbRow =  df.loc[df[LEMMA] == targetVerb]
+    # resultsDictionary = {}
+    # resultsDictionary["action"] = targetVerb
+    # #search for row which has the target verb
+    # targetVerbRow =  df.loc[df[LEMMA] == targetVerb]
 
-    #grab the ID of that verb at that row
-    targetVerbID = str(targetVerbRow[ID].iloc[0])
-    # dict = {"9":"nationals"}
-    # print getFullAgentFromChildNodes(getChildrenNodes(df, "9", dict))
+    # #grab the ID of that verb at that row
+    # targetVerbID = str(targetVerbRow[ID].iloc[0])
+    
+    # for index, row in df.iterrows():
+        
+        
+    #     srl = df.get_value(index, SRL)
+    #     twoOrMoreSRLArguments = str(srl).split(";") #3:A0=PAG;11:A0=PAG two or more arguments are separated by semicolon
+    #     oneSRLArgument = str(srl).split(":") #28:A0=PAG one argument separates relatedID and argument with colon
+    #     validSRLs = []
+    #     if len(twoOrMoreSRLArguments) > 1: #there are two or more arguments
+    #         validSRLs = twoOrMoreSRLArguments
+    #     elif len(oneSRLArgument) > 1: #one argument
+    #         validSRLs.append(srl)
 
+    #     for srlSection in validSRLs: #["3:A0=PAG", "11:A0=PAG"]
+    #         argumentSplit = srlSection.split(":")
+    #         relatedID = str(argumentSplit[0]) #3
+    #         argumentNumberFull = argumentSplit[1] #'A0=PAG'
+
+    #         if relatedID == targetVerbID: #our current row has an agent that corresponds to our target action
+    #             argumentNumber = argumentNumberFull.split("=")[0] # just want to extract the 'A0' from 'A0=PAG'
+    #             agentID = str(df.get_value(index, ID))  #the ID of the row of the SRL with the agent
+    #             agentWord = str(df.get_value(index, WORD))
+                
+    #             resultsDictionary[argumentNumber] = getFullAgent(df, agentID)
+                
     # print df
+    # print resultsDictionary
 
-    for index, row in df.iterrows():
-        srl = df.get_value(index, SRL)
-        twoOrMoreSRLArguments = str(srl).split(";") #3:A0=PAG;11:A0=PAG two or more arguments are separated by semicolon
-        oneSRLArgument = str(srl).split(":") #28:A0=PAG one argument separates relatedID and argument with colon
-        validSRLs = []
-        if len(twoOrMoreSRLArguments) > 1: #there are two or more arguments
-            validSRLs = twoOrMoreSRLArguments
-        elif len(oneSRLArgument) > 1: #one argument
-            validSRLs.append(srl)
-
-        for srlSection in validSRLs: #["3:A0=PAG", "11:A0=PAG"]
-            argumentSplit = srlSection.split(":")
-            relatedID = str(argumentSplit[0]) #3
-            argumentNumberFull = argumentSplit[1] #'A0=PAG'
-
-            if relatedID == targetVerbID: #our current row has an agent that corresponds to our target action
-                argumentNumber = argumentNumberFull.split("=")[0] # just want to extract the 'A0' from 'A0=PAG'
-                agentID = str(df.get_value(index, ID))  #the ID of the row of the SRL with the agent
-                agentWord = str(df.get_value(index, WORD))
-                
-                resultsDictionary[argumentNumber] = getFullAgent(df, agentID)
-                
-    print df
-    print resultsDictionary
-
-main()
-
-
+    main()
