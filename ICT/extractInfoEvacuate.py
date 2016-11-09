4419c0e73b498bc6850a0ff9df42a7b16316914e
Eamon Barkhordarian <ebarkhor@usc.edu>
2016-11-08 17:37:50 -0800

    added utils function to return a dictionary of file+sent to valid dataframes for a given target action

--- ICT/extractInfoEvacuate.py
+++ ICT/extractInfoEvacuate.py
@@ -2,6 +2,9 @@ import extractUtils
 import pandas as pd
 from io import StringIO
 import io
+import calendar
+from collections import defaultdict
+import os
 
 ID = "ID"
 WORD = "Word"
@@ -15,64 +18,86 @@ SRL = "SRL"
 A0 = "A0"
 A1= "A1"
 
+def addLocationToDictionary(resultsDictionary, location):
+    if resultsDictionary["Location"] == None:
+        resultsDictionary["Location"] = location
+    else:
+        resultsDictionary["Location"] = resultsDictionary.pop("Location")+ ", " + location
+    return resultsDictionary
 
 def main():
     targetVerb = "evacuate"
-    files = extractUtils.getFileNamesForVerb(targetVerb)
-    dict = extractUtils.getAbsolutePathForSrlFiles(files, "ClearnlpOutput")
+    fileAndSentToValidDF = extractUtils.getValidDataFrameDictForTargetAction(targetVerb)
 
-    sentences = []
-    for key in dict.keys():
-        file = io.open("/Users/eamonb/Documents/CS544/Projects/group project/CSCI544_final_project/ICT/ClearnlpOutput/ClearnlpOutput/Part9/newsText4934.txt.srl", "r", encoding='utf-8')
-        srlSentenceChunks = file.read().split("\n\n")
+    outputDictionary = {}
+    for fileAndSent in fileAndSentToValidDF.keys():
+        if fileAndSent == "newsText1234.txt_Sent13": #only because I want to see results of one file which is faster
+            df = fileAndSentToValidDF[fileAndSent]
+            resultsDictionary = {}
+            resultsDictionary["Location"] = None
+            resultsDictionary["A0"] = None
+            resultsDictionary["A1"] = None
+            resultsDictionary["date"] = None
+            resultsDictionary["action"] = targetVerb
 
-        for chunk in srlSentenceChunks[:-1]:
-            TESTDATA = StringIO(chunk)
-            df = pd.read_table(TESTDATA, names=[ID,WORD,LEMMA,POS,FEATURES,PARENT,DEPENDENCY_LABELS,SRL])
-            if targetVerb in df[LEMMA].tolist():
-                sentences.append(df)
-            break
-        break
-    
-    resultsDictionary = {}
-    resultsDictionary["action"] = targetVerb
-    #search for row which has the target verb
-    targetVerbRow =  df.loc[df[LEMMA] == targetVerb]
+            #search for row which has the target verb
+            targetVerbRow =  df.loc[df[LEMMA] == targetVerb]
 
-    #grab the ID of that verb at that row
-    targetVerbID = str(targetVerbRow[ID].iloc[0])
+            #grab the ID of that verb at that row
+            targetVerbID = str(targetVerbRow[ID].iloc[0])
 
-    locationStartingWords = ["from", "of", "in"]
+            #from Vietman, of China, in Serbia
+            locationStartingWords = ["from", "of", "in"]
 
-    for index, row in df.iterrows():
+            for index, row in df.iterrows():
+                wordValue = str(df.get_value(index, WORD))
+                wordId = str(df.get_value(index, ID))
 
-        wordValue = str(df.get_value(index, WORD))
-        wordId = df.get_value(index, ID)
-        if wordValue in locationStartingWords:
-            print extractUtils.getFullAgent(df, wordId)
+                srl = df.get_value(index, SRL)
+                twoOrMoreSRLArguments = str(srl).split(";") #3:A0=PAG;11:A0=PAG two or more arguments are separated by semicolon
+                oneSRLArgument = str(srl).split(":") #28:A0=PAG one argument separates relatedID and argument with colon
+                validSRLs = []
+                if len(twoOrMoreSRLArguments) > 1: #there are two or more arguments
+                    validSRLs = twoOrMoreSRLArguments
+                elif len(oneSRLArgument) > 1: #one argument
+                    validSRLs.append(srl)
 
-        srl = df.get_value(index, SRL)
-        twoOrMoreSRLArguments = str(srl).split(";") #3:A0=PAG;11:A0=PAG two or more arguments are separated by semicolon
-        oneSRLArgument = str(srl).split(":") #28:A0=PAG one argument separates relatedID and argument with colon
-        validSRLs = []
-        if len(twoOrMoreSRLArguments) > 1: #there are two or more arguments
-            validSRLs = twoOrMoreSRLArguments
-        elif len(oneSRLArgument) > 1: #one argument
-            validSRLs.append(srl)
+                for srlSection in validSRLs: #["3:A0=PAG", "11:A0=PAG"]
+                    argumentSplit = srlSection.split(":")
+                    relatedID = str(argumentSplit[0]) #3
+                    argumentNumberFull = argumentSplit[1] #'A0=PAG'
 
-        for srlSection in validSRLs: #["3:A0=PAG", "11:A0=PAG"]
-            argumentSplit = srlSection.split(":")
-            relatedID = str(argumentSplit[0]) #3
-            argumentNumberFull = argumentSplit[1] #'A0=PAG'
+                    if relatedID == targetVerbID:  # our current row has an agent that corresponds to our target action
+                        argumentNumber = argumentNumberFull.split("=")[0]  # just want to extract the 'A0' from 'A0=PAG'
+                        agentID = str(df.get_value(index, ID))  # the ID of the row of the SRL with the agent
 
-            if relatedID == targetVerbID: #our current row has an agent that corresponds to our target action
-                argumentNumber = argumentNumberFull.split("=")[0] # just want to extract the 'A0' from 'A0=PAG'
-                agentID = str(df.get_value(index, ID))  #the ID of the row of the SRL with the agent
-                agentWord = str(df.get_value(index, WORD))
+                        word = df.get_value(index, WORD)
+                        resultsDictionary[argumentNumber] = word
+                        resultsDictionary[argumentNumber] = extractUtils.getFullAgent(df, agentID)
+
+                #extract location from SRL
+                if "AM-LOC" in resultsDictionary:
+                    resultsDictionary["Location"] = resultsDictionary.pop('AM-LOC')
+                    #TODO: check if the corresponding AM-LOC has a geopolitical entity in it
+
+                if row[WORD] in locationStartingWords:
+                    resultsDictionary = addLocationToDictionary(resultsDictionary, extractUtils.getFullAgent(df, str(row[ID])))
+
+                # see if the word corresponds to any day/month and if yes add it to the date/location param
+                if row[WORD] in calendar.day_name:
+                    resultsDictionary["Date/Time"] = row[WORD]
+
+                if row[WORD] in calendar.month_name:
+                    resultsDictionary["Date/Time"] = row[WORD]
+
+                if row[WORD] in calendar.day_abbr:
+                    resultsDictionary["Date/Time"] = row[WORD]
+
+                if row[WORD] in calendar.month_abbr:
+                    resultsDictionary["Date/Time"] = row[WORD]
+
+            outputDictionary[fileAndSent] = resultsDictionary
                 
-                resultsDictionary[argumentNumber] = extractUtils.getFullAgent(df, agentID)
-                
-    # print df
-    # print resultsDictionary
+    print outputDictionary
 
 main()
--- ICT/extractUtils.py
+++ ICT/extractUtils.py
@@ -93,10 +93,14 @@ def getFullAgent(df, targetParentID):
         currentID = str(df.get_value(index, ID))
         if currentID == targetParentID: #a row's parent value is the ID of the agent we found
             targetWord = str(df.get_value(index, WORD))
-            dictionary[targetParentID] = targetWord
+            dictionary[int(targetParentID)] = targetWord
             childNodeDictionary = getChildNodes(df, targetParentID, dictionary)
 
     return getFullAgentFromChildNodes(childNodeDictionary)
+
+def removeFirstWord(agent):
+    agent.split(' ', 1)
+    return agent.split(' ', 1)[1]
 
 def getChildNodes(df, targetParentID, dictionary):   
     '''
@@ -107,12 +111,11 @@ def getChildNodes(df, targetParentID, dictionary):
             parentID = str(df.get_value(index, PARENT))
             currentID = str(df.get_value(index, ID))
             if parentID == targetParentID: #a row's parent value is the ID of the agent we found
-                dictionary[currentID] = str(df.get_value(index, WORD))
+                dictionary[int(currentID)] = str(df.get_value(index, WORD))
                 dictionary.update(getChildNodes(df, currentID, dictionary))
         return dictionary
     else:
         return dictionary
-
 
 def getFullAgentFromChildNodes(dictionary):
     '''
@@ -125,57 +128,27 @@ def getFullAgentFromChildNodes(dictionary):
         sortedValues.append(dictionary[key])
     return " ".join(sortedValues)
 
+def getValidDataFrameDictForTargetAction(targetAction) :
+    files = getFileNamesForVerb(targetAction)
+
+    fileAndSentToValidDF = {}
+    dict = getAbsolutePathForSrlFiles(files, "ClearnlpOutput")
+
+    for key in dict.keys():
+        file = io.open(dict[key], "r", encoding='utf-8')
+        srlSentenceChunks = file.read().split("\n\n")
+        
+        sentenceNumber = 0
+        for chunk in srlSentenceChunks[:-1]:
+            TESTDATA = StringIO(chunk)
+            df = pd.read_table(TESTDATA, names=[ID,WORD,LEMMA,POS,FEATURES,PARENT,DEPENDENCY_LABELS,SRL])
+            if targetAction in df[LEMMA].tolist():
+                fileAndSent = key + "_Sent" + str(sentenceNumber)
+                fileAndSentToValidDF[fileAndSent] = df
+            sentenceNumber +=1
+
+    return fileAndSentToValidDF
+
 def main():
-    # targetVerb = "evacuate"
-    # files = getFileNamesForVerb(targetVerb)
-    # dict = getAbsolutePathForSrlFiles(files, "ClearnlpOutput")
-
-    # sentences = []
-    # for key in dict.keys():
-    #     file = io.open("/Users/eamonb/Documents/CS544/Projects/group project/CSCI544_final_project/ICT/ClearnlpOutput/ClearnlpOutput/Part30/newsText17789.txt.srl", "r", encoding='utf-8')
-    #     srlSentenceChunks = file.read().split("\n\n")
-
-    #     for chunk in srlSentenceChunks[:-1]:
-    #         TESTDATA = StringIO(chunk)
-    #         df = pd.read_table(TESTDATA, names=[ID,WORD,LEMMA,POS,FEATURES,PARENT,DEPENDENCY_LABELS,SRL])
-    #         if targetVerb in df[LEMMA].tolist():
-    #             sentences.append(df)
-    #         break
-    #     break
-    
-    # resultsDictionary = {}
-    # resultsDictionary["action"] = targetVerb
-    # #search for row which has the target verb
-    # targetVerbRow =  df.loc[df[LEMMA] == targetVerb]
-
-    # #grab the ID of that verb at that row
-    # targetVerbID = str(targetVerbRow[ID].iloc[0])
-    
-    # for index, row in df.iterrows():
-        
-        
-    #     srl = df.get_value(index, SRL)
-    #     twoOrMoreSRLArguments = str(srl).split(";") #3:A0=PAG;11:A0=PAG two or more arguments are separated by semicolon
-    #     oneSRLArgument = str(srl).split(":") #28:A0=PAG one argument separates relatedID and argument with colon
-    #     validSRLs = []
-    #     if len(twoOrMoreSRLArguments) > 1: #there are two or more arguments
-    #         validSRLs = twoOrMoreSRLArguments
-    #     elif len(oneSRLArgument) > 1: #one argument
-    #         validSRLs.append(srl)
-
-    #     for srlSection in validSRLs: #["3:A0=PAG", "11:A0=PAG"]
-    #         argumentSplit = srlSection.split(":")
-    #         relatedID = str(argumentSplit[0]) #3
-    #         argumentNumberFull = argumentSplit[1] #'A0=PAG'
-
-    #         if relatedID == targetVerbID: #our current row has an agent that corresponds to our target action
-    #             argumentNumber = argumentNumberFull.split("=")[0] # just want to extract the 'A0' from 'A0=PAG'
-    #             agentID = str(df.get_value(index, ID))  #the ID of the row of the SRL with the agent
-    #             agentWord = str(df.get_value(index, WORD))
-                
-    #             resultsDictionary[argumentNumber] = getFullAgent(df, agentID)
-                
-    # print df
-    # print resultsDictionary
 
     main()
