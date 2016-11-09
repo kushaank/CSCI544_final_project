60de163280fe62ea15fd127c58edb5de94e46310
Eamon Barkhordarian <ebarkhor@usc.edu>
2016-11-09 01:00:47 -0800

    updated my two python scripts

--- ICT/extractInfoAllow.py
+++ ICT/extractInfoAllow.py
@@ -2,21 +2,10 @@ import extractUtils
 import pandas as pd
 from io import StringIO
 import io
+import col
 import calendar
 from collections import defaultdict
 import os
-
-ID = "ID"
-WORD = "Word"
-LEMMA = "Lemma"
-POS = "POS"
-FEATURES = "Features"
-PARENT = "Parent"
-DEPENDENCY_LABELS = "Dependency Labels"
-SRL = "SRL"
-
-A0 = "A0"
-A1= "A1"
 
 def main():
     targetVerb = "allow"
@@ -26,7 +15,7 @@ def main():
 
     outputDictionary = {}
     for fileAndSent in fileAndSentToValidDF.keys():
-        if fileAndSent == "newsText15497.txt_Sent12": #only because I want to see results of one file which is faster
+        if fileAndSent == "newsText15497.txt_Sent10": #only because I want to see results of one file which is faster
             df = fileAndSentToValidDF[fileAndSent]
             
             resultsDictionary = {}
@@ -34,39 +23,30 @@ def main():
                 resultsDictionary[category] = None
             resultsDictionary["Action"] = targetVerb
 
-            targetVerbRow =  df.loc[df[LEMMA] == targetVerb]
-            targetVerbID = str(targetVerbRow[ID].iloc[0])
+            targetVerbRow =  df.loc[df[col.LEMMA] == targetVerb]
+            targetVerbID = str(targetVerbRow[col.ID].iloc[0])
 
             #from Vietman, of China, in Serbia
             locationStartingWords = ["from", "of", "in"]
 
-            #get all SRL for the row
+            #get arguments for the target word
             resultsDictionary = extractUtils.getArgumentsForGivenID(df, targetVerbID, resultsDictionary)
 
             for index, row in df.iterrows():
                 # location
-                if row[WORD] in locationStartingWords:
-                    resultsDictionary = addLocationToDictionary(resultsDictionary, extractUtils.getFullAgent(df, str(row[ID])))
+                if row[col.WORD] in locationStartingWords:
+                    resultsDictionary = addLocationToDictionary(resultsDictionary, extractUtils.getFullAgent(df, str(row[col.ID])))
                 # if "AM-LOC" in resultsDictionary:
                 #     resultsDictionary["Location"] = resultsDictionary.pop('AM-LOC')
                 #     #TODO: check if the corresponding AM-LOC has a geopolitical entity in it
                 
                 #date
-                if row[WORD] in calendar.day_name:
-                    resultsDictionary["Date/Time"] = row[WORD]
-
-                if row[WORD] in calendar.month_name:
-                    resultsDictionary["Date/Time"] = row[WORD]
-
-                if row[WORD] in calendar.day_abbr:
-                    resultsDictionary["Date/Time"] = row[WORD]
-
-                if row[WORD] in calendar.month_abbr:
-                    resultsDictionary["Date/Time"] = row[WORD]
+                resultsDictionary = extractUtils.addDateToDictionary(row[col.WORD], resultsDictionary)
                 
                 if "A1" in resultsDictionary:
                     targetVerbID
 
+            #removing the unnecessary categories from the dictionary that aren't revlevant to 'allow'
             for result in resultsDictionary.keys():
                 if category not in evacuateCategories:
                     resultsDictionary.pop(category)
--- ICT/extractInfoEvacuate.py
+++ ICT/extractInfoEvacuate.py
@@ -5,18 +5,7 @@ import io
 import calendar
 from collections import defaultdict
 import os
-
-ID = "ID"
-WORD = "Word"
-LEMMA = "Lemma"
-POS = "POS"
-FEATURES = "Features"
-PARENT = "Parent"
-DEPENDENCY_LABELS = "Dependency Labels"
-SRL = "SRL"
-
-A0 = "A0"
-A1= "A1"
+import col
 
 def addLocationToDictionary(resultsDictionary, location):
     if resultsDictionary["Location"] == None:
@@ -31,8 +20,6 @@ def main():
 
     evacuateCategories = ["Location", "A0", "A1", "Date/Time", "Action"]
     
-    #newsText3702.txt_Sent0
-
     outputDictionary = {}
     for fileAndSent in fileAndSentToValidDF.keys():
         df = fileAndSentToValidDF[fileAndSent]
@@ -42,8 +29,8 @@ def main():
             resultsDictionary[category] = None
         resultsDictionary["Action"] = targetVerb
 
-        targetVerbRow =  df.loc[df[LEMMA] == targetVerb]
-        targetVerbID = str(targetVerbRow[ID].iloc[0])
+        targetVerbRow =  df.loc[df[col.LEMMA] == targetVerb]
+        targetVerbID = str(targetVerbRow[col.ID].iloc[0])
 
         #from Vietman, of China, in Serbia
         locationStartingWords = ["from", "of", "in"]
@@ -56,25 +43,16 @@ def main():
                 resultsDictionary["Reason"] = resultsDictionary.pop("AM-TMP")
 
             # location
-            if row[WORD] in locationStartingWords:
-                resultsDictionary = addLocationToDictionary(resultsDictionary, extractUtils.getFullAgent(df, str(row[ID])))
+            if row[col.WORD] in locationStartingWords:
+                resultsDictionary = addLocationToDictionary(resultsDictionary, extractUtils.getFullAgent(df, str(row[col.ID])))
             # if "AM-LOC" in resultsDictionary:
             #     resultsDictionary["Location"] = resultsDictionary.pop('AM-LOC')
             #     #TODO: check if the corresponding AM-LOC has a geopolitical entity in it
             
             #date
-            if row[WORD] in calendar.day_name:
-                resultsDictionary["Date/Time"] = row[WORD]
+            resultsDictionary = extractUtils.addDateToDictionary(row[col.WORD], resultsDictionary)
 
-            if row[WORD] in calendar.month_name:
-                resultsDictionary["Date/Time"] = row[WORD]
-
-            if row[WORD] in calendar.day_abbr:
-                resultsDictionary["Date/Time"] = row[WORD]
-
-            if row[WORD] in calendar.month_abbr:
-                resultsDictionary["Date/Time"] = row[WORD]
-            
+        #removing the unnecessary categories from the dictionary that aren't revlevant to 'evacuate'
         for result in resultsDictionary.keys():
             if category not in evacuateCategories:
                 resultsDictionary.pop(category)
